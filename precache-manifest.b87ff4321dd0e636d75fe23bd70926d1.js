c7532689487c979f6e7194f2cba0337f59a5cef0
Prashant <prashantbarahi@gmail.com>
2019-09-09 21:08:19 +0545

    Updates

---
+++ precache-manifest.b87ff4321dd0e636d75fe23bd70926d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "403b52f4bce733303ba57369115ba630",
+    "url": "/index.html"
+  },
+  {
+    "revision": "727bae4df9a0a1d89876",
+    "url": "/static/css/2.aa46e993.chunk.css"
+  },
+  {
+    "revision": "2feb0ce618bcb52c6550",
+    "url": "/static/css/main.868fb22c.chunk.css"
+  },
+  {
+    "revision": "727bae4df9a0a1d89876",
+    "url": "/static/js/2.27d09705.chunk.js"
+  },
+  {
+    "revision": "2feb0ce618bcb52c6550",
+    "url": "/static/js/main.302090c1.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "2b19350a5191a9122fe2e7867484d8cf",
+    "url": "/static/media/me.2b19350a.jpg"
+  }
+]);
